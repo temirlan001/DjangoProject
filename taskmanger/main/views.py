ad0e051ff8fcf63e3889a87df82acee6f9f2ddb7
Temirlan <timabal9@gmail.com>
2021-09-10 18:49:13 +0600

    taskmanger

---
+++ taskmanger/main/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.order_by('id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма не верна'
+
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/create.html', context)
